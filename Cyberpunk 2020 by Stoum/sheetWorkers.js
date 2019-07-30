8cddd6291bda889e42b986427c2767ccd40ca061
Andrey Tkach <tkachaa1@gmail.com>
2019-07-30 19:17:31 +0300

    ref mod calculation script added

--- Cyberpunk 2020 by Stoum/sheetWorkers.js
+++ Cyberpunk 2020 by Stoum/sheetWorkers.js
@@ -1,5 +1,5 @@
 
-on("sheet:opened", function() {
+on("change:movement change:body", function() {
     getAttrs(["movement", "body"], function(values) {
         console.log(values);
         let mv = values.movement*3;
@@ -22,7 +22,7 @@ on("sheet:opened", function() {
     });
 });
 
-on("sheet:opened change:repeating_armor", function(eventInfo){
+on("clicked:recalc-sp", function(eventInfo){
     console.log(eventInfo);
     getAttrs(["_reporder_repeating_armor"], function(values){
         console.log(values);
@@ -58,7 +58,76 @@ on("sheet:opened change:repeating_armor", function(eventInfo){
     });
 });
 
+on("sheet:opened", function calculateRefMod() {
+    getAttrs(["_reporder_repeating_armor", "_reporder_repeating_gear", "_reporder_repeating_cyber"], function(values) {
+        console.log(values);
+        let items = values._reporder_repeating_armor.split(",");
+        let armorEvAttrs = [];
+        let gearModAttrs = [];
+        let cyberModAttrs = [];
+        items.forEach(function (id) {
+            armorEvAttrs.push(repAttrName("armor", id, "ev"));
+        });
+
+        items = values._reporder_repeating_gear.split(",");
+        items.forEach(function (id) {
+            gearModAttrs.push(repAttrName("gear", id, "gear_mods"));
+        });
+
+        items = values._reporder_repeating_cyber.split(",");
+        items.forEach(function (id) {
+            cyberModAttrs.push(repAttrName("cyber", id, "cyber_mods"));
+        });
+
+        let allAttrs = [];
+        Array.prototype.push.apply(allAttrs, armorEvAttrs);
+        Array.prototype.push.apply(allAttrs, gearModAttrs);
+        Array.prototype.push.apply(allAttrs, cyberModAttrs);
+        allAttrs.push("reflex");
+
+        getAttrs(allAttrs, function(attrList) {
+            let totalRefMod;
+            let totalEv = 0;
+            let totalGearMod = 0;
+            let totalCyberMod = 0;
+            let i;
+            for(i = 0; i < armorEvAttrs.length;) {
+                totalEv += attrList[allAttrs[i++]]*1;
+            }
+
+            for(; i < armorEvAttrs.length + gearModAttrs.length;) {
+                let mods = parseMods(attrList[allAttrs[i++]]);
+                totalGearMod += mods.ref;
+            }
+
+            for(; i < armorEvAttrs.length + gearModAttrs.length + cyberModAttrs.length;) {
+                let mods = parseMods(attrList[allAttrs[i++]]);
+                totalCyberMod += mods.ref;
+            }
+
+            totalRefMod = -totalEv + totalGearMod + totalCyberMod;
+
+            setAttrs({
+                reflex_modified: attrList.reflex*1 + totalRefMod
+            });
+        })
+    });
+});
+
 function repAttrName(section, id, attr) {
     return "repeating_"+section+"_"+id+"_"+attr;
+}
+
+function parseMods(mods){
+    mods = mods.split(",").map(mod => mod.trim());
+    let result = {};
+    mods.forEach(function(mod){
+        mod = mod.split(" ");
+        let value = mod[0];
+        let attr = mod[1];
+        result[attr] = value*1;
+    });
+
+    return result;
 }
 
